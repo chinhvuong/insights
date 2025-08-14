2aee03e0967808edc7f46e46c8aac76e2e38b7f7
chinhvuong <113272179+chinhvuong@users.noreply.github.com>
2025-08-14 22:57:40 +0700

    Create zsh.sh

---
+++ setup/zsh.sh
@@ -0,0 +1,6 @@
+brew install zsh git curl && chsh -s $(which zsh) && \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
+sed -i '' 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc && \
+source ~/.zshrc
